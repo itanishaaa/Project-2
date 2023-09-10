a2727c557a814db5fa88014d81d23f6f14137803
itanishaaa <139465150+itanishaaa@users.noreply.github.com>
2023-09-11 00:31:58 +0530

    Modern.js

--- Filaura/Modern.js
+++ Filaura/Modern.js
@@ -1,37 +1,43 @@
 var images = [
-    "H1.jpg",
-    "H2.jpg",
-    "H3.jpg",
-    "H4.jpg"
+  "H1.jpg",
+  "H2.jpg",
+  "H3.jpg",
+  "H4.jpg"
 ];
 var headings = ["Fashion","Diamond","Oxidized","Gold"];
 console.log("JavaScript is running!");
 document.addEventListener("DOMContentLoaded", function () {
-    var bannertwo = document.getElementsByClassName("bannertwo");
-    var anchor = document.getElementById('link');
-    var length = images.length;
-    var i = 0;
+  var bannertwo = document.getElementsByClassName("bannertwo");
+  var anchor = document.getElementById('link');
+  var length = images.length;
+  var i = 0;
 
-    function rotateBackground() {
-        console.log("riot");
-        if (i >= length) {
-            i = 0;
-        }
-        bannertwo[0].style.backgroundImage = `url(${images[i]})`;
-        anchor.innerHTML = headings[i];
-        i++;
+  function rotateBackground() {
+      console.log("riot");
+      if (i >= length) {
+          i = 0;
+      }
+      bannertwo[0].style.backgroundImage = `url(${images[i]})`;
+      anchor.innerHTML = headings[i];
+      i++;
 
-        console.log(i);
+      console.log(i);
 
-        // Move the setInterval call outside of the rotateBackground function.
-        // Set an interval to call the rotateBackground function every 3000 milliseconds.
-    }
+      // Move the setInterval call outside of the rotateBackground function.
+      // Set an interval to call the rotateBackground function every 3000 milliseconds.
+  }
 
-    rotateBackground(); // Initial call
+  rotateBackground(); // Initial call
 
-    // Set an interval to call the rotateBackground function every 3000 milliseconds.
-    setInterval(rotateBackground, 3000);
+  // Set an interval to call the rotateBackground function every 3000 milliseconds.
+  setInterval(rotateBackground, 2000);
 });
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('body'),
+  smooth: true
+});
+
 
 
 

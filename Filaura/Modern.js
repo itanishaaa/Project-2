fcf11c57bae2be66bb88433f86ee6b6c253ca0de
Debleena Roy <debleenaroy2002@gmail.com>
2023-09-10 20:51:21 +0530

    slide

--- Filaura/Modern.js
+++ Filaura/Modern.js
@@ -1,21 +1,24 @@
-
+var images = [
+    "H1.jpg",
+    "H2.jpg",
+    "H3.jpg",
+    "H4.jpg"
+];
+var headings = ["Fashion","Diamond","Oxidized","Gold"];
 console.log("JavaScript is running!");
 document.addEventListener("DOMContentLoaded", function () {
     var bannertwo = document.getElementsByClassName("bannertwo");
-    var images = [
-        "H1.jpg",
-        "H2.jpg",
-        "H3.jpg",
-        "H4.jpg"
-    ];
+    var anchor = document.getElementById('link');
     var length = images.length;
     var i = 0;
 
     function rotateBackground() {
+        console.log("riot");
         if (i >= length) {
             i = 0;
         }
-        bannertwo[0].style.backgroundImage = 'url(' + images[i] + ')';
+        bannertwo[0].style.backgroundImage = `url(${images[i]})`;
+        anchor.innerHTML = headings[i];
         i++;
 
         console.log(i);
@@ -29,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Set an interval to call the rotateBackground function every 3000 milliseconds.
     setInterval(rotateBackground, 3000);
 });
-
 
 
 
